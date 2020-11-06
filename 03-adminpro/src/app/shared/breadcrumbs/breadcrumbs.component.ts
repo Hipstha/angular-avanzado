96055fa9a6a4d38a5bc31678429655dae69abd7f
Alejandro Cruz <alex_crz97@hotmail.com>
2020-11-05 22:48:44 -0600

    Final de sección 8

--- 03-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ 03-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivationEnd, Data, Router } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  public title: string;
+  public titleSubs$: Subscription;
 
-  ngOnInit(): void {
+  constructor( private router: Router ) {
+    this.titleSubs$ = this.getArgumentosRuta()
+      .subscribe( ({ title }) => {
+        this.title = title;
+        document.title = `AdminPro - ${ title }`;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta(): Observable<Data> {
+    return this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      filter( (event: ActivationEnd) => event.snapshot.firstChild === null ),
+      map( (event: ActivationEnd) => event.snapshot.data )
+    );
   }
 
 }
